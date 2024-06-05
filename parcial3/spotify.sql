a2ad6216aaad183eabd83fd848d0a7b53905240f
ChelssyZazuetaDiaz <159403913+ChelssyZazuetaDiaz@users.noreply.github.com>
2024-06-04 17:59:46 -0700

    Práctica #8 Inseción de datos en spotify 04/06/24

--- parcial3/spotify.sql
+++ parcial3/spotify.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-05-2024 a las 03:02:13
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 8.1.10
+-- Tiempo de generación: 05-06-2024 a las 02:55:22
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,6 +33,21 @@ CREATE TABLE `artista` (
   `nombre` varchar(100) NOT NULL,
   `descripcion` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `artista`
+--
+
+INSERT INTO `artista` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Kenia Os', 'ÁLBUM \"PINK AURA\" YA DISPONIBLE \r\n\r\nKenia Guadalupe Flores Osuna, mejor conocida como Kenia Os y \"La mexa que tiene a to\' el mundo loco\" & \"La OG\" nació en Mazatlán, Sinaloa, México el 15 de julio de 1999.\r\n\r\n'),
+(3, 'Natanael Cano', 'Cano nació en Hermosillo, en el estado de Sonora, en 2001. Aunque amó la música desde muy pequeño, sus padres no tenían idea de que esta se apoderaría por completo de ella. A los 13 años empezó a aprender por sí mismo a tocar la guitarra.'),
+(4, 'Joy', 'JOY de una banda de K-Pop, Red Velvet, lanza su primera carrera en solitario en mayo con un álbum especial <안녕 (Hello)>. Debutó en agosto de 2014 como miembro de Red Velvet y es amada por su voz fresca y clara, que brinda alegría a sus oyentes.'),
+(5, 'Stevie Nicks', 'Stevie Nicks proporcionó un contrapeso místico y esquivo al pop dulce y frenético y al blues básico de Fleetwood Mac, añadiendo espiritualidad y terror a los momentos más tiernos del grupo. Cuando se alejó del grupo después de Tusk.'),
+(6, 'Ariana Grande ', 'Originaria de Boca Raton, Florida, donde nació en 1993, hija del diseñador gráfico Edward Butera y Joan Grande, Grande comenzó a cantar y actuar a una edad temprana, apareciendo en producciones teatrales locales. En 2008, cuando tenía 15 años, consiguió el papel de Charlotte en la producción de Broa'),
+(7, 'Doris Day', 'Doris Mary Ann Kappelhoff nació el 3 de abril de 1922 en Cincinnati, Ohio. Su padre era profesor de música, director de coro y organista de iglesia. A su madre le encantaba la música popular, especialmente la música country. Tenía 12 años y Day vivía con su madre y su hermano mayor en Ohio.'),
+(8, 'Ella Fitzgerald', 'Reconocida mundialmente como \"La Primera Dama de la Canción\", Ella Fitzgerald es posiblemente la mejor vocalista de jazz de todos los tiempos. Bendecida con una voz muy resonante, un amplio rango y una elocución casi perfecta.'),
+(9, 'Snoop Dogg', 'Snoop Dogg, una de las figuras más icónicas que surgieron de la era del G-funk de principios de los 90, evolucionó más allá de sus inicios en el gangsta rap para convertirse en un adorable elemento de la cultura pop con incursiones en la televisión, el cine, el entrenamiento de fútbol y lucha libre.'),
+(10, 'Shakira', 'La cantautora colombiana Shakira ha vendido más de 80 millones de discos en todo el mundo y ganó numerosos premios, incluidos tres GRAMMY y once Latin GRAMMY.');
 
 -- --------------------------------------------------------
 
@@ -61,6 +77,20 @@ CREATE TABLE `canciones` (
   `foto` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `canciones`
+--
+
+INSERT INTO `canciones` (`id`, `nombre`, `genero_id`, `duracion`, `fecha`, `activo`, `foto`) VALUES
+(1, 'Malas Decisiones', 2, '02:19:00', '2022-10-26', 1, 'https://cdn2.excelsior.com.mx/800x600/filters:format(jpg):quality(75)/media/pictures/2024/03/13/3088'),
+(2, 'Rolling in the Deep', 2, '03:48:00', '2010-11-29', 1, 'foto 1'),
+(3, 'Por siempre', 2, '03:05:00', '2018-08-17', 1, 'foto 2'),
+(4, 'Tsunami', 2, '02:55:00', '2020-09-04', 1, 'foto 3'),
+(5, 'Te odio', 2, '02:24:00', '2020-10-26', 1, 'foto 4\r\n'),
+(6, 'Tu Peor Pesadilla', 2, '02:21:00', '2022-10-16', 1, 'foto 5'),
+(7, 'Es Así', 2, '03:14:00', '2024-04-24', 1, 'foto 6'),
+(8, 'Aún No Me He Ido', 2, '03:32:00', '2020-02-14', 1, 'foto 6');
+
 -- --------------------------------------------------------
 
 --
@@ -84,6 +114,34 @@ CREATE TABLE `generos` (
   `nombre` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `generos`
+--
+
+INSERT INTO `generos` (`id`, `nombre`) VALUES
+(1, 'Rock'),
+(2, 'Pop'),
+(3, 'Jazz'),
+(4, 'Funk'),
+(5, 'Reggaetón'),
+(6, 'Electrónica\r\n'),
+(7, 'Rap'),
+(8, 'Hip-Hop'),
+(9, 'Bachata'),
+(10, 'Salsa'),
+(11, 'Cumbia'),
+(12, 'K-pop'),
+(13, 'Clásica'),
+(14, 'Instrumental\r\n'),
+(15, 'Country'),
+(16, 'Contemporánea'),
+(17, 'Metal'),
+(18, 'Hyperpop'),
+(19, 'Indie'),
+(20, 'Flamenco'),
+(21, 'Latina'),
+(22, 'Punk');
+
 -- --------------------------------------------------------
 
 --
@@ -95,6 +153,15 @@ CREATE TABLE `membresias` (
   `descripcion` varchar(100) NOT NULL,
   `precio` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `membresias`
+--
+
+INSERT INTO `membresias` (`id`, `descripcion`, `precio`) VALUES
+(1, 'Premium Individual', 129),
+(2, ' Premium Familiar ', 199),
+(3, 'Premium para Estudiantes', 69);
 
 -- --------------------------------------------------------
 
@@ -137,6 +204,22 @@ CREATE TABLE `usuarios` (
   `correo` varchar(50) NOT NULL,
   `membresia_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `membresia_id`) VALUES
+(1, 'Chelssy Zazueta', 'zazuetadiazchelssy@gmail.com', 1),
+(2, 'Estrella Lugo', 'lugosánchezestrella@gmail.com', 2),
+(3, 'Alfredo López ', 'lopezdiazalfredo@gmail,com', 3),
+(4, 'Amanda Mendoza', 'mendozamorenoamanda@gmail.com', 1),
+(5, 'Adela Ochoa ', 'quinteroochoaadela@gmail.com', 1),
+(6, 'Valentina Aguilar', 'aguilarvalentina08@gmail.com', 2),
+(7, 'Oscar Del Rey', 'oscardelrey45@gmail.com', 3),
+(8, 'Azul Polanco', 'polancoazul300@hotmail.com', 2),
+(9, 'Miranda Comunica', 'comunicamirandaa@hotmail.com', 3),
+(10, 'Fernando Esquerra', 'esquerraescobarfernando@gmail.com', 1);
 
 --
 -- Índices para tablas volcadas
@@ -214,7 +297,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `artista`
 --
 ALTER TABLE `artista`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `artistas_canciones`
@@ -226,7 +309,7 @@ ALTER TABLE `artistas_canciones`
 -- AUTO_INCREMENT de la tabla `canciones`
 --
 ALTER TABLE `canciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `favoritos`
@@ -238,13 +321,13 @@ ALTER TABLE `favoritos`
 -- AUTO_INCREMENT de la tabla `generos`
 --
 ALTER TABLE `generos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `membresias`
 --
 ALTER TABLE `membresias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `playlists`
@@ -262,7 +345,7 @@ ALTER TABLE `playlists_canciones`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
