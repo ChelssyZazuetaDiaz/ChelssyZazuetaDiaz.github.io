5c5ff7ec2286599ce8cf1c3c14eff4eb9a4cac30
ChelssyZazuetaDiaz <159403913+ChelssyZazuetaDiaz@users.noreply.github.com>
2024-08-30 18:43:24 -0700

    Práctica #1 30/08/24

---
+++ basededatosweb/parcial1/holamundo.php
@@ -0,0 +1,18 @@
+<?php
+
+ $nombre = "Chelssy";
+ $numero = 4;
+ $numero2 = "4";
+
+ $total = $numero + $numero2;
+
+ echo "Total = " . $total;
+
+ echo "<h1> HOLA MUNDO </h1>";
+
+for($i=0; $i<10; $i++){
+    echo $i . "<br>";
+}
+ echo  "<h1 style=' color: purple;'> Chelssy Zazueta </h1>"; 
+
+?>
